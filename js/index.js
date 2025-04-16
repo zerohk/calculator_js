d46e3eeb4ed260ee8b3e9e37a867899275775b4e
buercheng-linux <www.1245731267@qq.com>
2025-04-16 14:59:35 +0800

    添加operate函数，接收operator,oprandA,operandB

--- js/index.js
+++ js/index.js
@@ -2,28 +2,34 @@
 // 
 let add = function (a, b) {
   if (isNaN(Number(a)) || isNaN(Number(b))) {
-    return('参数有误，请确认!');
+    return ('参数有误，请确认!');
   }
   return a + b;
 }
 
 let subtract = function (a, b) {
   if (isNaN(Number(a)) || isNaN(Number(b))) {
-    return('参数有误，请确认!');
+    return ('参数有误，请确认!');
   }
   return a - b;
 }
 
 let multiply = function (a, b) {
   if (isNaN(Number(a)) || isNaN(Number(b))) {
-    return('参数有误，请确认!');
+    return ('参数有误，请确认!');
   }
   return a * b;
 }
 
 let divide = function (a, b) {
   if (isNaN(Number(a)) || isNaN(Number(b)) || b == 0) {
-    return('参数有误，请确认!');
+    return ('参数有误，请确认!');
   }
   return a / b;
 }
+
+let operate = function (operator, oprandA, operandB) {
+  return operator(oprandA, operandB);
+}
+
+console.log(operate(add, 1, 2));
