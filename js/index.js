ede1e383aeb27675e390b1315a93cc9d9c73b4a2
buercheng-linux <www.1245731267@qq.com>
2025-04-16 14:57:16 +0800

    添加加减乘除基础操作

---
+++ js/index.js
@@ -0,0 +1,29 @@
+// 第一步实现基础的加减乘除运算
+// 
+let add = function (a, b) {
+  if (isNaN(Number(a)) || isNaN(Number(b))) {
+    return('参数有误，请确认!');
+  }
+  return a + b;
+}
+
+let subtract = function (a, b) {
+  if (isNaN(Number(a)) || isNaN(Number(b))) {
+    return('参数有误，请确认!');
+  }
+  return a - b;
+}
+
+let multiply = function (a, b) {
+  if (isNaN(Number(a)) || isNaN(Number(b))) {
+    return('参数有误，请确认!');
+  }
+  return a * b;
+}
+
+let divide = function (a, b) {
+  if (isNaN(Number(a)) || isNaN(Number(b)) || b == 0) {
+    return('参数有误，请确认!');
+  }
+  return a / b;
+}
